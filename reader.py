aa7200c5d07af8e48c8898de604c10e191012607
The Wise Salmon <86761699+Salmonmag@users.noreply.github.com>
2025-04-28 13:58:56 +0300

    עדכון

--- reader.py
+++ reader.py
@@ -1,1 +1,16 @@
+import json
+#TODO: ליבא את כל הקבצים הרלוונטים
+def load_comments(filepath:str)->list[dict]:
+    """"""
+    
 
+    #TODO:
+    # 1.להשלים את ההערות
+    # 2.להשלים פונקציה
+    # 3.להןסיף טיפול בחריגות
+    # 4.להשלים פונקציה
+
+class CommentLoader:
+    """"""
+
+#יישום: from reader import load_comments,CommentLoader

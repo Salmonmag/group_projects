6b117236198c948e431e1cad6589876de12fa618
Eitan-Charlet <eitanch@ac.sce.ac.il>
2025-04-28 19:56:31 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from reader import *
+from  models import *
+from processor import *
+from utils import *
+
+comments_list = load_comments('twitter_data.json')
+comments_object_list =list()
+users_list = list()
+for i in comments_list:
+    replyTo = i["reply_to"]
+    comment = i['comment']
+    comments_object_list.append(Comment(replyTo,comment))
+    users_list = list()
+dict_list = old_list_toNew_dict(comments_list).items()
+for x in dict_list:
+    username = x[0]
+    hisComments = x[1]
+    users_list.append(UserComments(username,hisComments))
+sentimentResult = list()
+for y in comments_object_list:
+    sentimentResult.append(analyze_sentiment(y))
+
+list_count_comments = list()
+list_positive_ratio = list()
+list_negative_ratio = list()
+list_average_word_count = list()
+for z in users_list:
+    list_count_comments.append(z.count_comments())
+    list_positive_ratio.append(z.positive_ratio())
+    list_negative_ratio.append(z.negative_ratio())
+    #list_average_word_count.append(z.average_word_count())
+
+print(list_positive_ratio)

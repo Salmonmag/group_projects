816fedccd682a12ca173d0f6684a856dedb0d032
Eitan-Charlet <eitanch@ac.sce.ac.il>
2025-04-28 16:54:28 +0300

    Update writer.py

--- writer.py
+++ writer.py
@@ -1,7 +1,10 @@
 import json
 from textwrap import indent
 
-def result(myList:list):
+def result(myList:list)->None:
+    if type(myList) != list:
+        return "Wrong type of attribute!"
     with open("C:/Users/eitanch/desktop/result.json",'a',encoding="utf-8") as file:
         for i in myList:
             json.dump(i,file,ensure_ascii=False,indent=4)
+        file.write(f"\n")

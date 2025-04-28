bbc2aafe357af0ca8405d715671b4ebc67984f3e
itaynowdel <105181558+itaynowdel@users.noreply.github.com>
2025-04-28 19:03:21 +0300

    Update processor.py

--- processor.py
+++ processor.py
@@ -1,18 +1,32 @@
-# twit_list= [{"reply_to": "@AndreCVentura", "comment": "Por isso que o Chega é  um partido de instabilidade e não estabilidade. Numa altura de responsabilidade e respeito pelos portugueses. Querem moções de censura e votam contra moção de confiança. "},
-# {"reply_to": "@AndreCVentura", "comment": "Prepara-te para fazer as malas. Isto se ainda lá estiverem após as eleições "}]
-# חשוב לשים לב!!!! התוכנה כרגע עובדת לפי הרשימה בשורות 1-2, צריך לעדכן אותה לקליטה מJSON
+import json
+from models import*
+from utils import*
+from reader import*
+twiter_list=load_comments("twitter_data.json")
+
+
+with open("special_characters.json", "r", encoding="utf-8") as file:
+    special_characters = json.load(file) #עבודה על קובץ json
+
+with open("negative_words.json", "r", encoding="utf-8") as file:
+    negative_words = json.load(file)
+
+with open("positive_words.json", "r", encoding="utf-8") as file:
+    positive_words = json.load(file)
+
+
 def old_list_toNew_dict(list: list)-> dict:
     comment_dict = {}
-    for i in range(len(twit_list)):
-        temp_dict = twit_list[i]
+    for i in range(len(list)):
+        temp_dict = list[i]
         if temp_dict["reply_to"] not in comment_dict: #אם המשתמש  לא נמצא במילון החדש
             comment_dict[temp_dict["reply_to"]] = [temp_dict["comment"]] #נייצר מפתח חדש במילון עם שם המתשמש והתגובה שהגיבו לו
         else: #במידה והמשתמש קיים כבר, נוסיף עוד תגובה לרשימת
             comment_dict[temp_dict["reply_to"]].append(temp_dict["comment"])
     return comment_dict
-    
+
 def analyze_sentiment(comment: Comment) -> str:
-    stringi = Comment.strip().splite(" ")
+    stringi = comment.text.strip().split(" ")
     positive_count = 0
     negitive_count = 0
     for word in stringi:
@@ -27,19 +41,22 @@ def analyze_sentiment(comment: Comment) -> str:
     else:
         return "neutral"
 
+
+
 def find_short_comments(list: list):
     short_list = []
-    for i in range(len(twit_list)): # עוברים כל םעם על שורה אחת מהרשימה
-        stringi = twit_list[i]["comment"].strip() #בכל מילון שאנחנו בודקים, נהפוך את התגובה לSTR 
-        if stringi.count(" ") + 1 < 5: #נבדוק את האורך של הרצועה, בעזרת ספירת הרווחים(+1) בשביל המילה האחרונה
-            short_list.append(stringi) # מוסיפים כל פעם שורה חדשה אם נמצא שהיא קטנה מחמש
+    for i in range(len(list)):
+        stringi = list[i]["comment"].strip()
+        if stringi.count(" ") + 1 < 5:
+            short_list.append(stringi)
     return short_list
+
 
 def find_emoji(list: list)-> list:
     emoji_list = []
     for i in range(len(list)):
         for x in special_characters["special_characters"]:
-            stat = utils.fun_function(list[i]["comment"], x)
+            stat = fun_function(list[i]["comment"], x)
             if stat == True:
                 emoji_list.append(list[i]["comment"])
     return emoji_list
@@ -55,3 +72,22 @@ def comment_lengh(list: list):
     for stringi in comment_sort_list:
         final_list.append(stringi[1])
     return final_list
+
+def most_positive_user(list:list):
+    update_dict = old_list_toNew_dict(list)
+    user_scores = {}
+    for user, comments in update_dict.items(): #עוברים על המילון ולוקחים את המפתח(שם) ורשימת התגובות
+        total_score = 0
+        for comment_text in comments: # עוברים על כל תגובה בנפרד בתוך הרשימת תגובות לכל משתמש
+            comment_obj = Comment(user, comment_text) #לוקחים את הערכים שיש לנו והופכים אותו לאובייקט
+            val = analyze_sentiment(comment_obj) # מכניסים את האוביקט שיש לנו לתוך הפונקציה בשביל לחשב אם התגובה חיובית או שלילית
+            if val == "positive":
+                total_score += 1 # אם התגובה חיובית מוספית למונה
+            elif val == "negative":
+                total_score -= 1 #אם שלילית מורידים מהמונה
+        user_scores[user] = total_score #מכניסים למילון תוצאות שלנו את המשתמש(מפתח) והערך הוא הציון הסופי
+
+    most_positive = max(user_scores, key=user_scores.get)
+    return most_positive
+test = most_positive_user(twiter_list)
+print(test)
